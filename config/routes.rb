00663595714c27314d89f1c32ff555cbca0d07f2
SakamotoMakishi <dauw.mf81@gmail.com>
2019-07-26 19:27:06 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :books
-  resources :products
+  resources :books do
+    resources :reviews
+  end
+  resources :products do
+    resources :reviews
+  end
   resources :users
+  resources :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_043352) do
+ActiveRecord::Schema.define(version: 2019_07_26_012546) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2019_07_25_043352) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.float "rate"
+    t.text "review"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
