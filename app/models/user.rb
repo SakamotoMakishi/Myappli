c168e9fc9a33633f9f81d82fcee7b37532419417
SakamotoMakishi <dauw.mf81@gmail.com>
2019-07-29 19:16:49 +0900

    login,sinup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
-  has_many :books
+  has_many :books #アバター画像の登録
+  has_one_attached :avatar
   mount_uploader :image, ImageUploader
   validates :email, presence: true
 end
